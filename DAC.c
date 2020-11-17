e27ab986071efcd52f2818d98257841e2b206ca3
Quinn Kleinfelter <qkleinfelter@qkleinfelter.com>
2020-11-17 13:10:32 -0500

    Project template

---
+++ DAC.c
@@ -0,0 +1,34 @@
+/*******************************************************************
+ * DAC.c
+ * Instructor: Devinder Kaur
+ * Runs on TM4C123
+ * Implementation of the 4-bit digital to analog converter
+ * Authors: Daniel Valvano,
+ *					Jonathan Valvano,
+ * 					Thomas Royko
+ * Student: Quinn Kleinfelter
+ * Section: 001/003
+ * Date:    11/17/2020
+ *
+ * Port B bits 3-0 have the 4-bit DAC
+ *******************************************************************/
+
+#include "DAC.h"
+#include "..//inc//tm4c123gh6pm.h"
+
+// **************DAC_Init*********************
+// Initialize 4-bit DAC 
+// Input: none
+// Output: none
+void DAC_Init(void){
+
+}
+
+
+// **************DAC_Out*********************
+// output to DAC
+// Input: 4-bit data, 0 to 15 
+// Output: none
+void DAC_Out(unsigned long data){
+  
+}
